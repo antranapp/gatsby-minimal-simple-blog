a2673a03057c25f202ce6f664be87da211a2101c
An Tran <tran.binhan@gmail.com>
2022-11-16 10:20:50 +0700

    Update cypress

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8000/',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+})
